a8f6413793b94845d06c55ac7b5754fe244026c6
Cuervolu <angel.cuervo187@gmail.com>
2024-07-19 17:50:12 -0400

    feat(shadcn): add toast, autoform, card and form components

--- src/app.vue
+++ src/app.vue
@@ -1,10 +1,13 @@
 <script lang="ts">
 // Supports weights 100-900
 import '@fontsource-variable/inter';
+import Toaster from '@/components/ui/toast/Toaster.vue'
+
 </script>
 
 <template>
   <div>
+    <Toaster />
     <NuxtLayout>
       <NuxtPage/>
     </NuxtLayout>
