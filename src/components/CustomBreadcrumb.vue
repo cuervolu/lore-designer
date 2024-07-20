93e1218e1419d575da3362e65b678dfbc280442d
Cuervolu <angel.cuervo187@gmail.com>
2024-07-20 14:28:51 -0400

    feat: Add CustomBreadcrumb component for breadcrumb navigation

---
+++ src/components/CustomBreadcrumb.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import {useI18n} from "vue-i18n";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList
+} from "~/components/ui/breadcrumb";
+
+const {t} = useI18n()
+const localePath = useLocalePath()
+
+interface Props {
+  // Key to i18n translation
+  from: string
+  to: string
+  oldRoute: string
+  newRoute: string
+}
+
+const {to, from, oldRoute, newRoute} = defineProps<Props>();
+</script>
+
+<template>
+  <Breadcrumb>
+    <BreadcrumbList>
+      <BreadcrumbItem>
+        <BreadcrumbLink :href="localePath(oldRoute)">{{ t(from) }}</BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator />
+      <BreadcrumbItem>
+        <BreadcrumbLink :href="localePath(newRoute)">{{ t(to) }}</BreadcrumbLink>
+      </BreadcrumbItem>
+    </BreadcrumbList>
+  </Breadcrumb>
+</template>
+
+<style scoped>
+
+</style>
