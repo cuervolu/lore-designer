026e02712d469d1b8f2503480a4e4fcbb021a460
Ángel Cuervo <angel.cuervo187@gmail.com>
2026-01-31 21:22:00 -0300

    feat: add VueVirtualScroller integration

--- apps/desktop/src/main.ts
+++ apps/desktop/src/main.ts
@@ -1,13 +1,17 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import VueVirtualScroller from 'vue-virtual-scroller';
+import App from './App.vue';
+import router from './router';
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
+import './assets/global.css';
+import '@fontsource-variable/inter';
+import '@fontsource-variable/fira-code';
 
-import App from './App.vue'
-import router from './router'
-import './assets/global.css'
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(createPinia());
+app.use(router);
+app.use(VueVirtualScroller);
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.mount('#app');
